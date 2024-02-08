522055a53770a82764771a339d08637fa560c47b
Natalia <natalia.valova@web.de>
2024-02-08 10:06:28 +0100

    map method

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,62 +3,63 @@ import './Dialog.css'
 import {NavLink} from "react-router-dom";
 
 type dialogItemProps = {
-    id: number
+    id?: number
     name: string
 }
 const DialogItem = (props: dialogItemProps) => {
+    let path = "/dialogs/" + props.id
 
     return <div className={"dialogs-items" + " " + "active"}>
-        <NavLink to={"/dialogs/" + props.id} className={"item"}>
+        <NavLink to={path} className={"item"}>
             {props.name}
         </NavLink>
-        </div>
-}
-const Message = (props: any) => {
-    return    <div className={"dialogs-message"}>
-        <div className={"dialogs-item"}>lorem ksmdlgkajlrkgjlöahbn</div>
     </div>
 }
-export const Dialogs = () => {
+const Message = (props: any) => {
+    return <div className={"dialogs-message"}>
+        <div className={"dialogs-item"}>{props.message}</div>
+    </div>
+}
+export const Dialogs = (props: dialogItemProps) => {
 
     let dialogsData = [
-        { id: 1, name: "Nata"},
-        { id: 2, name: "Kostja ", },
-        { id: 3, name: "Nick ", },
-        { id: 4, name: "Phil ", },
-        { id: 5, name: "Nella ", },
-        { id: 6, name: "Nora ", },
+        {id: 1, name: "Nata"},
+        {id: 2, name: "Kostja ",},
+        {id: 3, name: "Nick ",},
+        {id: 4, name: "Phil ",},
+        {id: 5, name: "Nella ",},
+        {id: 6, name: "Nora ",},
     ]
     let dialogsMessageData = [
-        { id: 1, name: "yo"},
-        { id: 2, name: "yo", },
-        { id: 3, name: "yo", },
-        { id: 4, name: "yo", },
-        { id: 5, name: "yo", },
-        { id: 6, name: "yo", },
+        {id: 1, message: "Hi"},
+        {id: 2, message: "yo",},
+        {id: 3, message: "yo",},
+        {id: 4, message: "yo",},
+        {id: 5, message: "yo",},
+        {id: 6, message: "yo",},
     ]
+
+    let dialogDaten =    dialogsData.map(d => {
+            return <DialogItem key={d.id}  name={d.name}/>
+        });
+
+
+
+     let messageDaten =   dialogsMessageData.map(m => {
+            return <Message key={m.id} message={m.message}/>
+        });
+
+
     return (
         <div className={"dialogs"}>
-            <div className={"dialogs-items" + " " + "active"}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
-
+            <div className={"blabla"}>
+                {dialogDaten}
             </div>
 
             <div className={"dialogs-message"}>
-                <Message message ={dialogsMessageData[0].name}/>
-                <Message message ={dialogsMessageData[1].name}/>
-                <Message message ={dialogsMessageData[2].name}/>
-                <Message message ={dialogsMessageData[3].name}/>
-                <Message message ={dialogsMessageData[4].name}/>
-                <Message message ={dialogsMessageData[5].name}/>
+                {messageDaten}
             </div>
 
         </div>
-    );
-};
-
+    )
+}
