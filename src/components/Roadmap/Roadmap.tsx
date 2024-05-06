76cf630aea71a78059889b252cbddde1ac365dc7
Natalia <natalia.valova@web.de>
2024-05-06 17:10:32 +0200

    optimized Routes

--- src/components/Roadmap/Roadmap.tsx
+++ src/components/Roadmap/Roadmap.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
-import {Profile} from "../Profile/profile";
-import {Message} from "../dialogs/Message/Messageitem";
-import {Dialogs} from "../dialogs/Dialogs";
-import {state} from "../../state";
-import Music from "../Music/Music";
-import News from "../News/News";
-import {Settings} from "../Settings/Settings";
+import {Routes, Route} from 'react-router-dom';
+import Profile from '../Profile/profile'; // Import your components
+import Music from '../Music/Music'; // Import your components
+import News from '../News/News'; // Import your components
+import {Settings}from '../Settings/Settings';
+import DialogsContainer from "../dialogs/DialogsContainer";
+import {UsersContainer} from "../Users/UsersContainer";
 
- export const Roadmap = (props: any) => {
-    return (
-        <Routes>
-           <Route path="/*"  caseSensitive={false} element={<div>Error</div>}/>
-            <Route path={"/dialogs"}  caseSensitive={false}  element={<Dialogs dialogs={state.dialogPage.dialogs} messages={state.dialogPage.messages}/>}  />
-            <Route path={"/profile"}  caseSensitive={false}  element={<Profile/>} />
+
+
+
+class Roadmap extends  React.Component {
+    render() {
+        return <Routes>
+            <Route path={"/profile"}  caseSensitive={false}  element={<Profile/>}/>
+            <Route path={"/message/:id"}  caseSensitive={false}  element={<DialogsContainer/>}/>
+            <Route path={"/message"}  caseSensitive={false}  element={<DialogsContainer/>}/>
             <Route path={"/news"}  caseSensitive={false}  element={<Music/>} />
             <Route path={"/music"}  caseSensitive={false}  element={<News/>} />
             <Route path={"/settings"}  caseSensitive={false}  element={<Settings/>} />
+            <Route path={"/users"}  caseSensitive={false}  element={<UsersContainer />} />
+
         </Routes>
-    );
-};
+    }
+}
+export default Roadmap;
 
